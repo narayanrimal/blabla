20cc1bd15e56cf25eb58b16bdc9032544586e413
darakrang <dara.krang@mavs.uta.edu>
2015-09-21 19:48:48 -0500

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/src/main/java/com/example/narayanrimal/blabla/MainActivity.java

--- app/src/main/java/com/example/narayanrimal/blabla/MainActivity.java
+++ app/src/main/java/com/example/narayanrimal/blabla/MainActivity.java
@@ -11,7 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        this is a third line test
+//        this is a third line test1
+        //hi there how you guys doing
+        System.out.print("Hello World");
+        System.out.println("wat up guys");
     }
 
     @Override
